5e2d0999f7fe30edc5b88fc8ded36718e09543fc
ines-lorquet <ines.lorquet@laplateforme.io>
2024-12-18 10:51:00 +0100

    Add job 4

---
+++ job4.sql
@@ -0,0 +1,14 @@
+SELECT * FROM nobel WHERE yr = 1986
+
+SELECT * FROM nobel WHERE yr = 1967 AND subject = 'Literature'
+
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein'
+
+SELECT *
+FROM nobel
+WHERE subject = 'Mathematic';
+
+SELECT COUNT(*) AS laureates_count
+FROM nobel
+WHERE subject = 'Mathematic';
+
