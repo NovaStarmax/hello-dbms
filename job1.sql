cbfb3cdc5ef1035dd2cebe50c8d11b6736070af5
ines-lorquet <ines.lorquet@laplateforme.io>
2024-12-19 15:56:22 +0100

    Add job 6

--- job1.sql
+++ job1.sql
@@ -1,3 +1,4 @@
+
 SELECT Population 
 FROM countrystats 
 WHERE Country = 'Germany '
@@ -9,3 +10,5 @@ WHERE Country IN ('Germany ', 'Sweden ', 'Norway ', 'Denmark ');
 SELECT Country 
 FROM countrystats 
 WHERE Area_sq_mi > '200000' AND Area_sq_mi < '300000'
+
+show tables
