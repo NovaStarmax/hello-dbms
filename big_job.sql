4c315baa7f07f844b33b2e039b2de99766f068dc
NovaStarmax <a.gobbe13@gmail.com>
2025-01-21 17:28:15 +0100

    Feat Question 3

--- big_job.sql
+++ big_job.sql
@@ -13,7 +13,9 @@ CREATE TABLE Country (
 );
 
 -- Chargement des données
-LOAD DATA INFILE '/Users/sulivanmoreau/La_plateforme/hello-dbms/carbon-footprint-data-country.csv'
+-- Inès, ici tu dois suivre le chemin (on a pas la même version sql mais tu as compris), 
+-- Déposer le csv au même endroit (dans Upload) et copier le chemin dans des simple <'> et non pas les doubles comme par défault <">.
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 9.1/Uploads/carbon-footprint-data-country.csv'
 INTO TABLE Country
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
@@ -26,18 +28,15 @@ IGNORE 1 ROWS
 SELECT * FROM Country LIMIT 10;
 
 
-
-
-
 CREATE TABLE World (
     id INT PRIMARY KEY AUTO_INCREMENT,
     region VARCHAR(100),
-    coal FLOAT,         -- Pourcentage d'utilisation du charbon
-    gas FLOAT,          -- Pourcentage d'utilisation du gaz
-    oil FLOAT,          -- Pourcentage d'utilisation du pétrole
-    hydro FLOAT,        -- Pourcentage d'utilisation de l'hydroélectrique
-    renewable FLOAT,    -- Pourcentage d'utilisation des énergies renouvelables
-    nuclear FLOAT       -- Pourcentage d'utilisation du nucléaire
+    coal FLOAT,         
+    gas FLOAT,          
+    oil FLOAT,          
+    hydro FLOAT,        
+    renewable FLOAT,    
+    nuclear FLOAT       
 );
 
 INSERT INTO World (region, coal, gas, oil, hydro, renewable, nuclear) VALUES
@@ -49,3 +48,48 @@ INSERT INTO World (region, coal, gas, oil, hydro, renewable, nuclear) VALUES
 ('North America', 35.7, 24.6, 1.0, 12.9, 6.6, 18.9),
 ('South Asia', 65.7, 9.1, 5.2, 11.6, 4.6, 2.8),
 ('Sub­Saharan Africa', 51.4, 8.6, 4.3, 21.2, 1.7, 3.0);
+
+--3 
+
+-- Classement des régions selon leur dépendance aux énergies fossiles
+SELECT region, ROUND(coal + gas + oil, 2) AS fossil_dependency
+FROM World
+ORDER BY fossil_dependency DESC;
+
+-- Régions les plus axées sur les énergies renouvelables
+SELECT region, renewable
+FROM World
+ORDER BY renewable DESC;
+
+-- Comparaison entre énergies fossiles et propres par région
+SELECT region, 
+       ROUND(coal + gas + oil, 2) AS fossil, 
+       ROUND(hydro + renewable + nuclear, 2) AS clean
+FROM World
+ORDER BY fossil DESC;
+
+-- Top 5 des pays avec la plus grande utilisation de charbon
+SELECT country, coal
+FROM Country
+ORDER BY coal DESC
+LIMIT 5;
+
+-- Contribution des émissions totales de CO2 par région en pondérant chaque source d'énergie
+SELECT region, 
+       ROUND(coal * 820 + gas * 490 + oil * 740 + hydro * 24 + renewable * 41 + nuclear * 12, 2) AS total_carbon_emission
+FROM World
+ORDER BY total_carbon_emission DESC;
+
+-- Répartition énergétique par type d'énergie dans chaque région
+SELECT region, 
+       ROUND(coal / (coal + gas + oil + hydro + renewable + nuclear) * 100, 2) AS coal_percentage,
+       ROUND(gas / (coal + gas + oil + hydro + renewable + nuclear) * 100, 2) AS gas_percentage,
+       ROUND(oil / (coal + gas + oil + hydro + renewable + nuclear) * 100, 2) AS oil_percentage
+FROM World;
+
+-- Top 7 des pays les plus accès sur les energies renouvelables
+SELECT country, renewable
+FROM country
+ORDER BY renewable DESC
+LIMIT 7;
+
